6f6526b8f63509a7e40c5970dbee02fe9257ee7c
Abigail <68978544+a61gail@users.noreply.github.com>
2025-02-28 18:39:38 -0600

    OK Confirmación 2 Ríos

--- app/src/main/java/com/example/proyectoiiadminrios/MainActivity.java
+++ app/src/main/java/com/example/proyectoiiadminrios/MainActivity.java
@@ -1,19 +1,45 @@
 package com.example.proyectoiiadminrios;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button btnVenta, btnContacto, btnProdcutosServicios;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//inicia onCreate
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-    }
-}
+        //integración de xml a Java
+        btnVenta=findViewById(R.id.btnVenta);
+        //evento para abrir la nueva ventana
+        btnVenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abrirVenta = new Intent(getApplicationContext(), Venta.class) ;
+                startActivity(abrirVenta);
+            }
+        });
+        btnContacto=findViewById(R.id.btnContacto);
+        btnContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abrirContacto = new Intent(getApplicationContext(), Contacto.class) ;
+                startActivity(abrirContacto);
+            }
+        });
+        btnProdcutosServicios=findViewById(R.id.btnProductosServicios);
+        btnContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abrirProductosServicios = new Intent(getApplicationContext(), Producto.class) ;
+                startActivity(abrirProductosServicios);
+            }
+        });
+    }//termina método onCreate
+}//termina clase
